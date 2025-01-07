91e64625bc5516dbb5b4e309b78544a49b41fbc0
wuwverse global <wuwverseglobal@gmail.com>
2025-01-07 02:07:46 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.onload = function() {
     alert("Welcome to Terredyme, The Next Contech!");
 }
+
+const menuLinks = document.querySelectorAll('nav ul li a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
